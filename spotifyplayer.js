7c6b4770f128b9effc218e6710ea47ed8843bb5b
Eli <55drk1@gmail.com>
2015-08-07 14:19:37 +0200

    Third iteration

--- spotifyplayer.js
+++ spotifyplayer.js
@@ -1,6 +1,7 @@
 function getSong(event){
 	event.preventDefault();
 
+	$('#progress-bar').attr('value', 0)
 	function searchString(){
 		search = $('#search').val();
 		return searchString = search.replace(" ","+");
@@ -12,7 +13,9 @@ function getSong(event){
 		
 		var thesong = songs.tracks.items[0]
 		$('.title').text(thesong.name)
-		$('.author').text(thesong.artists[0].name)
+		$('.author').text('<button type="button" class="btn" data-toggle="modal" data-target="#myModal">
+  '+ thesong.artists[0].name +
+'</button>')
 		$('.cover').html('<img src="' + thesong.album.images[0].url + '">')
 		$('.audiodiv').html('<audio src="'+ thesong.preview_url +'" class="js-player"></audio>')
 	}
@@ -28,6 +31,7 @@ function getSong(event){
 $('#searchform').submit(getSong);
 
 function playActions(){	
+
 	if ($('.btn-play').hasClass('playing')){
 		$('.js-player').trigger('pause');
 		$('.btn-play').removeClass('playing');
@@ -35,6 +39,16 @@ function playActions(){
 		$('.js-player').trigger('play');
 		$('.btn-play').addClass('playing');
 	}
+
+	$('.js-player').on('timeupdate', printTime);
 }
 
 $('.btn-play').on('click',playActions);
+
+
+function printTime () {
+  var current = $('.js-player').prop('currentTime');
+  $('#progress-bar').attr('value', current)
+  console.log('Current time: ' + current);
+}
+
